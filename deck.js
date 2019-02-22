55fa2777366e969430f8d60e5ebab1a491fc05b4
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-22 14:55:07 -0800

    initial commit - created class layout

---
+++ deck.js
@@ -0,0 +1,24 @@
+class DeckCards {
+  constructor() {
+    this.cards = [];
+    this.suit = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    this.values = [
+      'Ace',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'Jack',
+      'Queen',
+      'King'
+    ];
+  }
+
+  createDeck() {
+    this.deck = [];
+  }
+}
