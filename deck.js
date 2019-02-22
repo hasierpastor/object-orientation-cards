3af628215ca257757b8df4e9e72bd377e1fd241c
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-22 15:11:42 -0800

    created shuffle method

--- deck.js
+++ deck.js
@@ -2,6 +2,7 @@ class DeckCards {
   constructor() {
     this.deck = [];
     this.createDeck();
+    this.shuffle();
   }
 
   createDeck() {
@@ -27,6 +28,18 @@ class DeckCards {
         this.deck.push({ [suit]: value });
       }
     }
+  }
+
+  shuffle() {
+    let { deck } = this;
+    //implementation of Fisher Yates Shuffle from https://bost.ocks.org/mike/shuffle/
+    let i = deck.length;
+    let j;
+    while (i) {
+      j = Math.floor(Math.random() * i--);
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
   }
 }
 
